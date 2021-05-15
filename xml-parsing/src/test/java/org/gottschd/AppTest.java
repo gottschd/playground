75abb5e72e1e4a9a10a62a04957e0577971116a9
Danny Gottschling <Gottschling.Danny@akdb.de>
2021-05-15 16:16:10 +0200

    enable the other test

--- xml-parsing/src/test/java/org/gottschd/AppTest.java
+++ xml-parsing/src/test/java/org/gottschd/AppTest.java
@@ -1,16 +1,14 @@
 package org.gottschd;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
 import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
     @Test
-    @Disabled
     public void parseCDataXml() throws Exception {
         try (InputStream in = this.getClass().getResourceAsStream("/req_cdata_embedded_small.xml")) {
             new OuterXmlParser().parse(in);
